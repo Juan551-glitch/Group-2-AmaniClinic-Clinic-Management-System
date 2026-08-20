af846f748b4fe2f6536f1e265f0f8c0038713689
Evan528 <evanheyman9@gmail.com>
2026-08-20 13:43:05 +0200

    Add client-side validation for name input fields
    Added name-input CSS class to name fields in Doctors.cshtml, Patients.cshtml, and Appointment.cshtml. Updated site.js to restrict input to letters (including accented), spaces, hyphens, and apostrophes in real time for these fields.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,9 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Restrict name fields to letters and the punctuation commonly used in names.
+document.querySelectorAll(".name-input").forEach((input) => {
+    input.addEventListener("input", () => {
+        input.value = input.value.replace(/[^A-Za-zÀ-ÿ' -]/g, "");
+    });
+});
